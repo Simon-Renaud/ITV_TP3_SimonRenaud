06f4aaf826881993aee57d7739a7ead3058740ed
Simon Renaud <2196370@csfoy.ca>
2022-10-08 21:39:26 -0400

    Ajout des fichiers

---
+++ serveur1/html/index.php
@@ -0,0 +1,16 @@
+<h1>Test de connexion - Serveur 1</h1>
+<h4>Tentative de connexion Mysql depuis PHP...</h4>
+
+<?php
+    $host = 'mysql';
+    $user = 'root';
+    $pass = 'rootpassword';
+    $conn = new mysqli ($host, $user, $pass);
+    
+    if ($conn->connect_error)
+    {
+        die("La connexion à échoué: " . $conn->connect_error);
+    }
+    
+    echo "Connexion réussie à mysql!";
+?>
